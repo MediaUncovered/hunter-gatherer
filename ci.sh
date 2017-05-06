4a681ae75198d7630016f23be436f40e1e43a58e
Theo van den Bogaart <todorus@todorus.com>
2017-05-06 17:18:10 +0200

    #4 added bash script for continous integration

---
+++ ci.sh
@@ -0,0 +1,8 @@
+# get the local directory to convert relative paths to absolute ones
+LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# make sure we use the latest builds
+docker-compose -f $LOCAL_DIR/deployment/test.yaml build --no-cache
+
+# run the tests
+docker-compose -f $LOCAL_DIR/deployment/test.yaml up --abort-on-container-exit --force-recreate
