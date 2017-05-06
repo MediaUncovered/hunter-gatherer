169c61583766afc326e79f0d4ffdde1105e11439
Theo van den Bogaart <todorus@todorus.com>
2017-05-06 17:55:34 +0200

    #2 added script for development environment

--- develop.sh
+++ develop.sh
@@ -1,8 +1,10 @@
 # get the local directory to convert relative paths to absolute ones
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# make sure we use the latest build
-docker-compose -f $LOCAL_DIR/deployment/develop.yaml build
-
-# run the development container
-docker-compose -f $LOCAL_DIR/deployment/develop.yaml up
+#
+# # make sure we use the latest build
+# docker-compose -f $LOCAL_DIR/deployment/develop.yaml build
+#
+# # run the development container
+# docker-compose -f $LOCAL_DIR/deployment/develop.yaml up
+docker build . -f deployment/app/Dockerfile -t todorus/news_analysis-hunter_gatherer:production
+docker run -it -v $LOCAL_DIR/app:/app todorus/news_analysis-hunter_gatherer:production /bin/bash
