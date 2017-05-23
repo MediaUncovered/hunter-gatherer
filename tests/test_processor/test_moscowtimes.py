f2ac5522e0d5524c772e35aad24b2ede3a7a95e3
Theo van den Bogaart <todorus@todorus.com>
2017-05-23 19:37:01 +0200

    #8 tests no longer cheat

--- tests/test_processor/test_moscowtimes.py
+++ tests/test_processor/test_moscowtimes.py
@@ -15,8 +15,8 @@ class TestMoscowTimesProcessing(unittest.TestCase):
         self.subject = Processor()
 
         # Given an article page of the Moscow Times
-        self.source_url = "https://www.nytimes.com/reuters/2017/05/22/us/politics/22reuters-usa-budget.html"
-        test_data_path = os.path.join(test_dir_path, 'data/nytimes_sample.html')
+        self.source_url = "http://old.themoscowtimes.com/sitemap/free/2014/1/article/netherlands-extradites-russian-accused-of-overseeing-shipments-of-cocaine-stashed-in-bananas/492579.html"
+        test_data_path = os.path.join(test_dir_path, 'data/moscowtimes_sample.html')
         with open(test_data_path, 'r', encoding='cp1251') as f:
             self.html_data = f.read().replace('\n', '')
 
@@ -26,12 +26,13 @@ class TestMoscowTimesProcessing(unittest.TestCase):
         result = self.subject.process(self.source_url, self.html_data)
 
         # Then it extracts the article content
-        body_data_path = os.path.join(test_dir_path, 'data/ny_body.txt')
+        body_data_path = os.path.join(test_dir_path, 'data/moscowtimes_body.txt')
         with open(body_data_path, 'r', encoding='utf-8') as f:
             self.body = f.read()
         self.assertEquals(self.body, result.body)
         # And its metadata
-        expected_title = "Trump Budget Poised to Slash Healthcare for Poor, Other Programs"
+        # expected_title = "Netherlands Extradites Russian Accused of Overseeing Shipments of Cocaine Stashed in Bananas"
+        expected_title = "Netherlands Extradites Russian Accused of Overseeing Shipments of Cocaine Stashed in Bananas"
         self.assertEquals(expected_title, result.title)
-        expected_datetime = datetime.datetime(2017, 5, 22)
+        expected_datetime = datetime.datetime(2014, 1, 14)
         self.assertEquals(expected_datetime, result.datetime)
