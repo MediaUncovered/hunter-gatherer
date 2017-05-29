99d5d63d0708addcaab9271b10bd53987da7f95e
Theo van den Bogaart <todorus@todorus.com>
2017-05-29 22:54:49 +0200

    #8 made progress on the ny times queries

--- tests/test_processor/test_processing_nytimes.py
+++ tests/test_processor/test_processing_nytimes.py
@@ -4,35 +4,53 @@ Tests for analyzing and labeling a Moscow Times article
 import unittest
 import os
 import datetime
+from app.config import Query
 from app.processor import Processor
 
 test_dir_path = os.path.dirname(__file__)
 
 
-@unittest.skip("PENDING")
 class TestProcessingNyTimes(unittest.TestCase):
 
     def setUp(self):
-        self.subject = Processor()
+        self.maxDiff = None
 
-        # Given an article page of the Moscow Times
-        self.source_url = "https://www.nytimes.com/reuters/2017/05/22/us/politics/22reuters-usa-budget.html"
+        title_queries = [
+            Query(
+                query="//article//h1/text()"
+            )
+        ]
+        body_queries = [
+            Query(
+                query="//article//div[contains(concat(' ', @class, ' '), ' story-body ')]//p//text()"
+            )
+        ]
+        subject = Processor(title_queries=title_queries,
+                            body_queries=body_queries)
+
+        # Given an article page of the NY Times
+        source_url = "https://www.nytimes.com/reuters/2017/05/22/us/politics/22reuters-usa-budget.html"
         test_data_path = os.path.join(test_dir_path, 'data/nytimes_sample.html')
         with open(test_data_path, 'rb') as f:
-            self.html_data = f.read()
-
-    def test_processing(self):
+            html_data = f.read()
 
         # When the article is processed by the Processor class
-        result = self.subject.process(self.source_url, self.html_data)
+        self.result = subject.process(source_url, html_data)
 
-        # Then it extracts the article content
+    def test_title(self):
+        # Then it extracts its title
+        expected_title = "Trump Budget Poised to Slash Healthcare for Poor, Other Programs"
+        self.assertEquals(expected_title, self.result.title)
+
+    def test_date(self):
+        # Then it extracts the publication date
+        expected_datetime = datetime.datetime(2017, 5, 22)
+        self.assertEquals(expected_datetime, self.result.datetime)
+
+    def test_body(self):
+        # Then it extracts content
         body_data_path = os.path.join(test_dir_path, 'data/ny_body.txt')
         with open(body_data_path, 'r', encoding='utf-8') as f:
-            self.body = f.read()
-        self.assertEquals(self.body, result.body)
-        # And its metadata
-        expected_title = "Trump Budget Poised to Slash Healthcare for Poor, Other Programs"
-        self.assertEquals(expected_title, result.title)
-        expected_datetime = datetime.datetime(2017, 5, 22)
-        self.assertEquals(expected_datetime, result.datetime)
+            body = f.read()
+        # print("\n\nexpected\n%r\n\nresult\n%r" % (body, self.result.body))
+        self.assertEquals(body, self.result.body)
