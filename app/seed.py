8486cfb0a9da832f5d13647980bf62c67f0b2853
Theo van den Bogaart <todorus@todorus.com>
2017-10-18 00:24:07 +0200

    #18 ny times definition

--- app/seed.py
+++ app/seed.py
@@ -12,9 +12,13 @@ print("Inserting Moscow Times")
 moscow_times_source = model.Source(name="Moscow Times")
 session.add(moscow_times_source)
 session.commit()
+print("NY Moscow Times")
+ny_times_source = model.Source(name="New York Times")
+session.add(moscow_times_source)
+session.commit()
 
 print("Seeding first task")
-pipe_definition_path = "moscow_times_all.yaml"
+pipe_definition_path = "ny_times_archive.yaml"
 # pipe_definition_path = "moscow_times_day.yaml"
 with open(pipe_definition_path) as stream:
     pipe_definition = yaml.load(stream, Loader=yaml.Loader)
