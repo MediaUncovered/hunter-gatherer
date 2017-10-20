c547f678ef25b7ba6977e625999f766097eacf57
Theo van den Bogaart <todorus@todorus.com>
2017-10-20 16:39:44 +0200

    #18 paginate returns source_id for following jobs

--- app/jobs/paginate.py
+++ app/jobs/paginate.py
@@ -5,11 +5,13 @@ def run(arguments):
     print("paginate %r" % arguments)
     pattern = arguments.get("paginate_pattern")
     page = arguments.get("paginate_page")
+    source_id = arguments.get("source_id")
     url = re.sub(r"{page}", "%i" % page, pattern)
 
     result = {
         "paginate_pattern": pattern,
         "paginate_page": page + 1,
         "url": url,
+        "source_id": source_id,
     }
     return [result]
--- tests/test_pager/test_pager.py
+++ tests/test_pager/test_pager.py
@@ -20,6 +20,7 @@ class TestPagination(unittest.TestCase):
             "paginate_pattern": "https://query.nytimes.com/search/sitesearch/#/*/from19810101to20171231/document_type%3A%22article%22/{page}/allauthors/oldest/",
             "paginate_page": 2,
             "url": "https://query.nytimes.com/search/sitesearch/#/*/from19810101to20171231/document_type%3A%22article%22/1/allauthors/oldest/",
+            "source_id": 1,
         }]
 
         self.assertEquals(expected, result)
@@ -38,6 +39,7 @@ class TestPagination(unittest.TestCase):
             "paginate_pattern": "https://query.nytimes.com/search/sitesearch/#/*/from19810101to20171231/document_type%3A%22article%22/{page}/allauthors/oldest/",
             "paginate_page": 6,
             "url": "https://query.nytimes.com/search/sitesearch/#/*/from19810101to20171231/document_type%3A%22article%22/5/allauthors/oldest/",
+            "source_id": 1,
         }]
 
         self.assertEquals(expected, result)
